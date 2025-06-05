3d3393b0747790303e7a1cbca1f84aeb02bc4903
RuddyBHD <ruddy_delacruz@bhd.com.do>
2025-06-04 23:14:40 -0400

    Primer commit de mi proyecto Spring Boot

---
+++ src/main/java/com/api/sicom/api/ApiSicomSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.api.sicom.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSicomSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSicomSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/sicom/api/ApiSicomSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sicom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSicomSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
