23fb70dfe0f42c29f5d0b5391f7df27cbf6bf458
RuddyBHD <ruddy_delacruz@bhd.com.do>
2025-06-10 20:51:29 -0400

    Cors

--- src/main/java/com/api/sicom/api/config/CorsConfig.java
+++ src/main/java/com/api/sicom/api/config/CorsConfig.java
@@ -1,10 +1,6 @@
 package com.api.sicom.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Permite todos los orígenes
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
